48df5c27f0bd2a1854750d13c76f46f3127a5397
AgneBalc <agne.martinaityte@gmail.com>
2023-11-22 16:43:46 +0200

    custom colors added to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,18 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      purple: "#AD1FEA",
+      blue: "#4661E6",
+      darkBlue: "#373F68",
+      white: "#FFFFFF",
+      lightGray: "#F2F4FF",
+      extraLightGray: "#F7F8FD",
+      gray: "#3A4374",
+      mediumGray: "#647196",
+      orange: "#F49F85",
+      skyBlue: "#62BCFA",
+    },
     extend: {},
   },
   plugins: [],
