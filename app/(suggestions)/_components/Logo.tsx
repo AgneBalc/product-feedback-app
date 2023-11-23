ad57f823bba9400e70b9e3cfc5929a2bf8c026a1
AgneBalc <agne.martinaityte@gmail.com>
2023-11-23 11:01:49 +0200

    Logo styles created for Sidebar component

--- app/(suggestions)/_components/Logo.tsx
+++
@@ -1,5 +0,0 @@
-const Logo = () => {
-  return <div className="relative bg-desktop w-64 h-[137px] rounded-md"></div>;
-};
-
-export default Logo;
--- app/(suggestions)/_components/Sidebar.tsx
+++ app/(suggestions)/_components/Sidebar.tsx
@@ -1,11 +1,27 @@
+import Image from "next/image";
 import Filter from "./Filter";
-import Logo from "./Logo";
 import RoadmapInfo from "./RoadmapInfo";
 
 const Sidebar = () => {
   return (
     <div>
-      <Logo />
+      <div className="h-[72px] sm:w-56 sm:h-44 lg:w-64 lg:h-[137px] text-white w-full">
+        <div className="bg-mobile lg:bg-desktop sm:bg-tablet sm:rounded-md bg-cover w-full h-full flex justify-between sm:items-end items-center px-6 sm:pb-6">
+          <div>
+            <h2>Frontend Mentor</h2>
+            <span className="text-[13px] sm:text-base opacity-75">
+              Feedback Board
+            </span>
+          </div>
+          <Image
+            src="/shared/mobile/icon-hamburger.svg"
+            alt="Menu button"
+            width={20}
+            height={17}
+            className="block sm:hidden cursor-pointer"
+          />
+        </div>
+      </div>
       <Filter />
       <RoadmapInfo />
     </div>
