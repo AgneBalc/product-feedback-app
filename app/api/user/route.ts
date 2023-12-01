5611716519501ab75180fb4bf0afd4b1da498352
AgneBalc <agne.martinaityte@gmail.com>
2023-12-01 16:24:39 +0200

    UserValidator created

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { db } from "@/lib/db";
 import { hash } from "bcrypt";
+import { z } from "zod";
 
 export const POST = async (req: Request) => {
   try {
---
+++ lib/validators/user.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+
+export const UserValidator = z
+  .object({
+    username: z
+      .string({ required_error: "Username is required" })
+      .trim()
+      .min(3, { message: "Username must be at least 3 characters long." })
+      .max(30, {
+        message: "Username must be be less than 30 characters long.",
+      }),
+    name: z
+      .string({ required_error: "Name is required" })
+      .min(3, { message: "Name must be at least 3 characters long." })
+      .max(100, { message: "Name must be be less than 100 characters long." }),
+    email: z
+      .string({ required_error: "Email is required" })
+      .trim()
+      .email({ message: "Invalid email address" }),
+    image: z.string().trim().url({ message: "Invalid url" }),
+    password: z
+      .string({ required_error: "Password is required" })
+      .min(6, { message: "Password must be at least 6 characters long." }),
+    confirmPassword: z.string({
+      required_error: "Password confirmation is required",
+    }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "Passwords do not match",
+  });
